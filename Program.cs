70e94877d0450e732a3ca57288f73b1497bb0e45
ahad raza <96381905+ahadraza2021@users.noreply.github.com>
2021-12-31 23:48:47 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+using System;
+public static class Program
+{
+    public static void Main()
+    {
+        using (PdfDocument document = PdfDocument.Open(@"C:\Users\AHAD RAZA\Downloads\AHAD RAZA NUML (1).pdf"))
+        {
+            foreach (Page page in document.GetPages())
+            {
+                string pagetext = page.Text;
+                foreach(Word word in page.GetWords())
+                {
+                    Console.WriteLine(word.Text);
+                }
+            }
+        }
+    }
+}
